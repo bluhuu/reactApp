f5818932c86fc21b3c02a5084f5238c0155a7b37
bluhuu <bluhuu@gmail.com>
2016-05-11 16:02:52 +0800

    Signed-off-by: bluhuu <bluhuu@gmail.com>

---
+++ App.jsx
@@ -0,0 +1,15 @@
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            
+            Hello World!!!
+            Hello World!!!
+            Hello World!!!
+            Hello World!!!Hello World!!!Hello World!!!
+         </div>
+      );
+   }
+}
+
+export default App;
